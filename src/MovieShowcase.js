e9a28b11a065ad687f4de3e2f0bd6bba3f0f88d8
Omer Sabah <omerrj99@gmail.com>
2021-06-07 00:58:35 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
-import MovieCard from './card-components/MovieCard.js';
-import movieData from './data.js';
+
 
 export default class MovieShowcase extends Component {
 
